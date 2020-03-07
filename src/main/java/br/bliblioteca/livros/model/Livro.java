a5433c55d728376e7332d9ef9583153bc1dc0bcc
A18 <A18@192.168.7.129>
2020-03-07 16:30:18 -0300

    ajuste dos autores

--- src/main/java/br/bliblioteca/livros/model/Livro.java
+++ src/main/java/br/bliblioteca/livros/model/Livro.java
@@ -10,9 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "LIVRO")
@@ -24,12 +21,8 @@ public class Livro implements Serializable {
 	@GeneratedValue
 	private Long id;
 
-	@NotNull
-	@Size(min = 2, max = 100)
 	private String nome;
 
-	@NotNull
-	@Min(10)
 	private int quantidadePaginas;
 
 	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
