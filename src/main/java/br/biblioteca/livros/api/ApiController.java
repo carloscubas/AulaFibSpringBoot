0da52e2fa2129de677e4a5fb1edefc1d98051a99
s2it_csilva <s2it_csilva@uolinc.com>
2020-04-18 11:26:50 -0300

    inclusao api

---
+++ src/main/java/br/biblioteca/livros/api/ApiController.java
@@ -0,0 +1,30 @@
+package br.biblioteca.livros.api;
+
+import static br.biblioteca.livros.conversor.LivroConverter.toDTO;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.biblioteca.livros.dto.LivroDTO;
+import br.biblioteca.livros.model.Livro;
+import br.biblioteca.livros.service.LivrosService;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+	@Autowired
+	LivrosService livrosService;
+
+	@GetMapping("/livros/list")
+	public ResponseEntity<List<LivroDTO>> livros() {
+		List<Livro> listaLivros = livrosService.listaTodosLivros();
+		return ResponseEntity.ok(toDTO(listaLivros));
+	}
+
+}
