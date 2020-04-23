b292d1aa89f7a0bf3fc7e40b2cc36a76524035ba
s2it_csilva <s2it_csilva@uolinc.com>
2020-04-23 20:19:23 -0300

    inclusao da avaliacao

--- src/main/java/br/biblioteca/livros/api/ApiController.java
+++ src/main/java/br/biblioteca/livros/api/ApiController.java
@@ -7,9 +7,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.biblioteca.livros.dto.AvaliacaoDTO;
 import br.biblioteca.livros.dto.LivroDTO;
 import br.biblioteca.livros.model.Livro;
 import br.biblioteca.livros.service.LivrosService;
@@ -28,6 +32,13 @@ public class ApiController {
 	public ResponseEntity<List<LivroDTO>> livros() {
 		List<Livro> listaLivros = livrosService.listaTodosLivros();
 		return ResponseEntity.ok(toDTO(listaLivros));
+	}
+
+	@PostMapping("/livro/avaliacao/{id}")
+	public ResponseEntity<Long> comentario(@PathVariable("id") Long id, @RequestBody AvaliacaoDTO avaliacaoDTO) {
+
+		return ResponseEntity.ok(1l);
+
 	}
 
 }
